1830a4b30acf0b262a89fe38387493ba913c073b
fancybearIN <itsdeep0007@gmail.com>
2024-11-14 13:04:31 +0530

    git h

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <title>HOSPITAL</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="style.css"> 
     <style>
         body {
                 background-color: white; /* Set background color to white */
